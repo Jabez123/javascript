aec59d5019aad262b055cd0d98b779700edbe04d
Jabez123 <it.jabez16@gmail.com>
2017-02-22 14:54:49 +0800

    Add files via upload

---
+++ Javascript 101/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Activity 5</title>
+  </head>
+  <body>
+    <script language="javascript" type="text/javascript">
+      <!--
+        document.write("Hello World");
+
+        var1 = 10
+        var2 = 20
+        sum = var1 + var2
+        document.write(sum)
+      //-->
+    </script>
+  </body>
+</html>
